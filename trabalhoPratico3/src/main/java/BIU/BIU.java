b6c77036e831f32719900b056e0806a4953282c2
DIEGOSOUSA <sdiego478@gmail.com>
2022-06-13 19:40:45 -0100

    classe
    atualizando as classes

--- trabalhoPratico3/src/main/java/BIU/BIU.java
+++ trabalhoPratico3/src/main/java/BIU/BIU.java
@@ -1,11 +1,14 @@
 package BIU;
 
+import BIU.USUARIO.Titular;
+
 abstract public class BIU {
     private static int codigo;
     private String data;
     private String dataValidade;
     private double saldo;
     private String chaveValicacao;
+    Titular portador=new Titular();
 
     public void setSaldo(double saldo) {
         this.saldo = saldo;
@@ -31,6 +34,35 @@ abstract public class BIU {
 
     public String getChaveValicacao() {
         return chaveValicacao;
+    }
+
+    public void setchaveValidacao(){
+        String[] nome=portador.getNome().split(" ");
+        String Nome=nome[0];
+        String Apelido=nome[1];
+        this.chaveValicacao=criarChaveConfirmacao(Nome,Apelido);
+
+    }
+    private String criarChaveConfirmacao(String primeiroNome, String segundoNome){
+        String chave=devider(primeiroNome)+devider(segundoNome)+this.data+this.codigo;
+        return chave;
+    }
+    private String devider(String conteudo){
+        String nomere=" ";
+        char[] intermediario = new char[5];
+
+        if (conteudo.length() <= 5) {
+            nomere=conteudo;
+        } else {
+            conteudo.getChars((conteudo.length()-5), conteudo.length(), intermediario, 0);
+            nomere = new StringBuilder(String.valueOf(intermediario)).reverse().toString();
+        }
+        return upperCaseFirst(nomere);
+    }
+    public static String upperCaseFirst(String val) {
+        char[] arr = val.toCharArray();
+        arr[0] = Character.toUpperCase(arr[0]);
+        return new String(arr);
     }
 
 
