08838d319410bc85dd59471a4eaa2006947e34b2
DIEGOSOUSA <sdiego478@gmail.com>
2022-06-23 16:47:15 -0100

    mudancas
    ad

--- trabalhoPratico3/src/main/java/BIU/bui/Rota.java
+++ trabalhoPratico3/src/main/java/BIU/bui/Rota.java
@@ -1,11 +1,20 @@
-package BIU;
+package BIU.bui;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Rota {
     private HashMap<String,String> ilhas=new HashMap<String,String>();
+    private String partida;
+    private String destino;
+
     public void addRota(String partida,String destino){
         ilhas.put(this.partida,this.destino);
     }
+    public Rota(HashMap<String, String> ilhas) {
+        this.ilhas = ilhas;
+    }
+
+
 }
