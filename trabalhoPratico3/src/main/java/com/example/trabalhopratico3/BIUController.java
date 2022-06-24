6f2adf1d619ceb3519e478b7b4dd99e6e0d2c3ac
DIEGOSOUSA <sdiego478@gmail.com>
2022-06-24 10:20:09 -0100

    grafica
    inicio da implementacao da coponenete grafica

---
+++ trabalhoPratico3/src/main/java/com/example/trabalhopratico3/BIUController.java
@@ -0,0 +1,12 @@
+package com.example.trabalhopratico3;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class BIUController {
+
+    @FXML
+    public void login(){}
+    @FXML
+    public void cadastrar(){}
+}
---
+++ trabalhoPratico3/src/main/java/com/example/trabalhopratico3/LoginController.java
@@ -0,0 +1,6 @@
+package com.example.trabalhopratico3;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class LoginController {
+}
