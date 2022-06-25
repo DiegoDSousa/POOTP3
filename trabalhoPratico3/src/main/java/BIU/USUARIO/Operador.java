2f99c04f9e02a34c11cec8ce98bb6e3225865240
DIEGOSOUSA <sdiego478@gmail.com>
2022-06-25 16:15:33 -0100

    teste
    a

--- trabalhoPratico3/src/main/java/BIU/USUARIO/Operador.java
+++ trabalhoPratico3/src/main/java/BIU/USUARIO/Operador.java
@@ -1,4 +1,34 @@
 package BIU.USUARIO;
 
 public class Operador {
+<<<<<<< Updated upstream
+=======
+    private String codigo;
+    private String nome;
+    private String morada;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMorada() {
+        return morada;
+    }
+
+    public void setMorada(String morada) {
+        this.morada = morada;
+    }
+>>>>>>> Stashed changes
 }
